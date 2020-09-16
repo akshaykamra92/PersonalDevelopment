b29c3bfbd29ffaa38ec4f3fb5f9bdc23265acc0c
Karishma Chawla <kkamra@uab.edu>
2020-09-16 15:38:47 -0500

    check duplicates in array

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,12 +1,12 @@
-class Solution:
-    def containsDuplicate(self, nums) -> bool:
-        nums.sort()
-        for i in range(len(nums)-1):
-            if (nums[i] == nums[i + 1]) :
-                return 1
-            else:
-                pass
-        return 0
+
+def containsDuplicate(nums):
+    nums.sort()
+    for i in range(len(nums)-1):
+        if (nums[i] == nums[i + 1]) :
+            return 1
+        else:
+            pass
+    return 0
 nums = [1,2,3,1]
 print(containsDuplicate(nums))
 
