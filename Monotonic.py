15e850b70e8f1978ab48c36c93146e067884e9f2
akshaykamra <akshaykamra92@gmail.com>
2021-04-14 22:22:59 -0500

    Test

--- Monotonic.py
+++ Monotonic.py
@@ -1,5 +1,5 @@
 # List = [1,1,2,3,4,5]
-List = [5,4,3,2,1,6]
+List = [1,2,3]
 
 test = all(List[i] <= List[i+1] for i in range(len(List)-1)) or all(List[i] >= List[i+1] for i in range(len(List)-1))
 
